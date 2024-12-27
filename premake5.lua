d1a38dcc63a1a0d9714db496557ba7014d1f4630
Leyan Cui <leyancui@foxmail.com>
2024-12-27 15:21:32 +0800

    build: create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/tmp/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+	}
+
+	buildoptions {
+		"/utf-8",
+	}
+
+	filter "system:linux"
+		pic "On"
+		cdialect "Default"
+		staticruntime "On"
+		systemversion "latest"
+
+	filter "system:windows"
+		cdialect "Default"
+		staticruntime "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
